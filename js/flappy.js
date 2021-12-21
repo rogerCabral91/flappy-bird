8f76bcb5fc87b5e9aac1c0b4b035dec69538fe52
Roger Cabral <roger_cabral@hotmail.com>
2021-12-21 20:25:04 -0300

    add Barrier constructor

--- js/flappy.js
+++ js/flappy.js
@@ -1,5 +1,17 @@
 function newElement(tagName, className) {
-    const elem = document.createElement(tagName)
-    elem.className = className
-    return elem
+  const customElement = document.createElement(tagName);
+  customElement.className = className;
+  return customElement;
 }
+
+function Barrier(reverse = false) {
+  this.element = newElement("div", "barrier");
+
+  const barrierBorder = newElement("div", "barrier-edge");
+  const barrierBody = newElement("div", "barrier-body");
+  this.element.appendChild(reverse ? barrierBody : barrierBorder);
+  this.element.appendChild(reverse ? barrierBorder : barrierBody);
+
+  this.setHeight = (height) => (barrierBody.style.height = `${height}px`);
+}
+
