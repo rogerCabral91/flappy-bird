393e0544564459c7fc33f3f1dfab0d44ce2eee3d
Roger Cabral <roger_cabral@hotmail.com>
2021-12-21 20:48:52 -0300

    add constructor PairOfBarriers

--- js/flappy.js
+++ js/flappy.js
@@ -15,3 +15,29 @@ function Barrier(reverse = false) {
   this.setHeight = (height) => (barrierBody.style.height = `${height}px`);
 }
 
+function PairOfBarriers(height, opening, x) {
+  this.element = newElement("div", "pair-of-barriers");
+
+  this.top = new Barrier(true);
+  this.bottom = new Barrier(false);
+
+  this.element.appendChild(this.top.element);
+  this.element.appendChild(this.bottom.element);
+
+  this.drawOpening = () => {
+    const upperHeight = Math.random() * (height - opening);
+    const lowerHeight = height - opening - upperHeight;
+    this.top.setHeight(upperHeight);
+    this.bottom.setHeight(lowerHeight);
+  };
+
+  this.getX = () => parseInt(this.element.style.left.split("px")[0]);
+  this.setX = (x) => (this.element.style.left = `${x}px`);
+  this.getWidth = () => this.element.clientWidth;
+
+  this.drawOpening()
+  this.setX(x)
+}
+
+const b = new PairOfBarriers(720, 220, 400)
+document.querySelector('[fb-flappy]').appendChild(b.element)
